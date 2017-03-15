0ff68f009ea005481e50c367e22e14bdf86f4722
songhejia <851232853@qq.com>
2017-03-15 18:58:31 +0800

    加入 $.connection.hub

---
+++ SignalR/SignalR.WX/SignalR.WeiXin.js
@@ -0,0 +1,21 @@
+﻿$(function () {
+    var weiXin = $.connection.weiXin;
+
+    function login(userid) {
+        weiXin.server.login(userid).done(function (data) {
+            console.log(data);
+        });
+    }
+
+    $.extend(weiXin.client, {
+        getContactsList: function(data) {
+            console.log(data);
+        }
+    });
+
+    // Start the connection
+    $.connection.hub.start().done(function () {
+        login("2568952");
+        login("sss");
+    });
+})
--- SignalR/SignalR.WX/WeiXinHub.cs
+++ SignalR/SignalR.WX/WeiXinHub.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using Newtonsoft.Json;
 
 namespace SignalR.SignalR.WX
 {
+    [HubName("weiXin")]
     public class WeiXinHub : Hub
     {
         public static Dictionary<string, User> LoginUsers;
@@ -17,22 +20,54 @@ namespace SignalR.SignalR.WX
             _weiXin = new WeiXin();
         }
 
-
+        /// <summary>
+        /// 登录
+        /// </summary>
+        /// <param name="userId"></param>
         public void Login(string userId)
         {
             var user = _weiXin.GetUser(userId);
+            user.userId = userId;
             user.connectionId = Context.ConnectionId;
 
             if (!LoginUsers.ContainsKey(userId))
             {
-                //TODO 添加
+                //TODO: 添加
                 LoginUsers.Add(userId, user);
             }
             else
             {
-                //TODO 更新
+                //TODO: 更新
                 LoginUsers[userId] = user;
             }
+
+            GetContactsList(user);
+        }
+
+        /// <summary>
+        /// 发送DevicesList
+        /// </summary>
+        /// <param name="user"></param>
+        public void GetContactsList(User user)
+        {
+            //GET http://{$API_HOST}/contacts/list?token={$token}&imei={$imei}
+            //输入参数:
+            //  imei:对应的设备imei
+            //返回结果:
+            //{
+            //  'status':1,
+            //  'records': [
+            //    {
+            //      'wxid'		: '{好友wxid}',
+            //            'nickname': '{好友昵称}',
+            //      //其他信息字段待定
+            //    },...
+            //  ]
+            //}
+            //TODO：调用接口 /devices/list
+            var jsonData = JsonConvert.SerializeObject(user);
+            Clients.Client(user.connectionId).getContactsList(jsonData);
+
 
         }
 
